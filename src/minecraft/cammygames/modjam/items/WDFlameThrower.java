8dae2b3a39ff2d0d8a72d8d2d0e7211d18fb9f5e
cammygames <cammygames@icloud.com>
2013-12-13 20:06:00 +0000

    More FLame Thrower work

--- src/minecraft/cammygames/modjam/items/WDFlameThrower.java
+++ src/minecraft/cammygames/modjam/items/WDFlameThrower.java
@@ -3,8 +3,8 @@ package cammygames.modjam.items;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
-import net.minecraftforge.common.MinecraftForge;
 import cammygames.modjam.WarDefence;
 
 public class WDFlameThrower  extends Item
@@ -17,19 +17,33 @@ public class WDFlameThrower  extends Item
 
 	public boolean lavaBucket = false;
 	private int countparticles = 0;
-	
 	/**
      * Called whenever this item is equipped and the right mouse button is pressed. Args: itemStack, world, entityPlayer
      */
-    public ItemStack onItemRightClick(ItemStack ItemStack, World world, EntityPlayer par3EntityPlayer)
-    {
-    	for(int countparticles = 0; countparticles <= 10; ++countparticles)
+	public boolean onItemUse(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World world, int par4, int par5, int par6, int par7, float par8, float par9, float par10)
+	{
+		int direction = MathHelper.floor_double((double)((par2EntityPlayer.rotationYaw * 4F) / 360F) + 0.5D) & 3;
+		private EntityFireball;
+		
+    	if (par2EntityPlayer.inventory.hasItem(Item.bucketLava.itemID)) 
     	{
-    	        world.spawnParticle("smoke", par3EntityPlayer.posX, par3EntityPlayer.posY, par3EntityPlayer.posZ, 0.0D, 0.0D, 0.0D); //spawns the particle "smoke"
+    		/*Consume a fire ball*/
+    		par2EntityPlayer.inventory.consumeInventoryItem(Item.bucketLava.itemID);
+    		
+    		lavaBucket = true;
+    		
+    		while (lavaBucket)
+    		{
+    			for(countparticles = 0; countparticles <= 10; ++countparticles) 
+    			{
+    				world.spawnParticle("smoke", par2EntityPlayer.posX, par2EntityPlayer.posY, par2EntityPlayer.posZ, 0.0D, 0.0D, 0.0D);
+					world.spawnEntityInWorld(EntityFireball);
+				}
+    		}
+    		 
     	}
-
-    	par3EntityPlayer.setItemInUse(ItemStack, this.getMaxItemUseDuration(ItemStack)); //Use it like a Sword: Block attacks
-    	return ItemStack; //Return the expected ItemStack.
-    }
-	
+		
+		return true;
+	}
+		
 }
