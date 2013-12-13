b171c9eba0e14751c0fe6b299ec62c3bc2fab553
cammygames <cammygames@icloud.com>
2013-12-13 20:22:03 +0000

    More work

--- src/minecraft/cammygames/modjam/items/WDFlameThrower.java
+++ src/minecraft/cammygames/modjam/items/WDFlameThrower.java
@@ -1,11 +1,16 @@
 package cammygames.modjam.items;
 
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.ArrowLooseEvent;
+import net.minecraftforge.event.entity.player.ArrowNockEvent;
 import cammygames.modjam.WarDefence;
+import cammygames.modjam.entity.FireBall;
 
 public class WDFlameThrower  extends Item
 {
@@ -17,33 +22,55 @@ public class WDFlameThrower  extends Item
 
 	public boolean lavaBucket = false;
 	private int countparticles = 0;
-	/**
-     * Called whenever this item is equipped and the right mouse button is pressed. Args: itemStack, world, entityPlayer
-     */
-	public boolean onItemUse(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World world, int par4, int par5, int par6, int par7, float par8, float par9, float par10)
+
+	 @Override
+    public void onUsingItemTick(ItemStack itemStack, EntityPlayer player, int count) 
 	{
-		int direction = MathHelper.floor_double((double)((par2EntityPlayer.rotationYaw * 4F) / 360F) + 0.5D) & 3;
-		private EntityFireball;
-		
-    	if (par2EntityPlayer.inventory.hasItem(Item.bucketLava.itemID)) 
-    	{
-    		/*Consume a fire ball*/
-    		par2EntityPlayer.inventory.consumeInventoryItem(Item.bucketLava.itemID);
-    		
-    		lavaBucket = true;
-    		
-    		while (lavaBucket)
-    		{
-    			for(countparticles = 0; countparticles <= 10; ++countparticles) 
-    			{
-    				world.spawnParticle("smoke", par2EntityPlayer.posX, par2EntityPlayer.posY, par2EntityPlayer.posZ, 0.0D, 0.0D, 0.0D);
-					world.spawnEntityInWorld(EntityFireball);
-				}
-    		}
-    		 
-    	}
-		
-		return true;
+		World world = player.worldObj;
+	    boolean flag = player.capabilities.isCreativeMode || EnchantmentHelper.getEnchantmentLevel(Enchantment.infinity.effectId, itemStack) > 0;
+	
+	    if (flag || player.inventory.hasItem(Item.fireballCharge.itemID)) {
+	        double yaw = Math.toRadians(player.rotationYaw);
+	        double pitch = Math.toRadians(player.rotationPitch);
+	
+	        double xDirection = -Math.sin(yaw) * Math.cos(pitch);
+	        double yDirection = -Math.sin(pitch);
+	        double zDirection = Math.cos(yaw) * Math.cos(pitch);
+	
+	        double x = player.posX + xDirection * 2;
+	        double y = player.posY + 1.5 + yDirection;
+	        double z = player.posZ + zDirection * 2;
+	
+	        FireBall fireball = new FireBall(world, x, y, z, xDirection, yDirection, zDirection);
+	
+	        int power = EnchantmentHelper.getEnchantmentLevel(Enchantment.power.effectId, itemStack);
+	        if (power > 0) {
+	            fireball.setDamage(fireball.getDamage() + (double) power * 0.5D + 0.5D);
+	            if (power == 5) {
+	                fireball.setExplosive(true);
+	            }
+	        }
+	
+	        int knockback = EnchantmentHelper.getEnchantmentLevel(Enchantment.punch.effectId, itemStack);
+	        if (knockback > 0) {
+	            fireball.setKnockbackStrength(knockback);
+	        }
+	
+	        if (EnchantmentHelper.getEnchantmentLevel(Enchantment.flame.effectId, itemStack) > 0) {
+	            fireball.setFire(100);
+	        }
+	
+	        itemStack.damageItem(1, player);
+	        world.playAuxSFX(1009, (int) x, (int) y, (int) z, 0);
+	
+	        if (!flag) {
+	            player.inventory.consumeInventoryItem(Item.fireballCharge.itemID);
+	        }
+	
+	        if (!world.isRemote) {
+	            world.spawnEntityInWorld(fireball);
+	        }
+	    }
 	}
-		
+	
 }
