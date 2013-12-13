538f654b0eaf5f16f5e088dd530e4c598d540d8b
cammygames <cammygames@icloud.com>
2013-12-13 20:41:29 +0000

    More

--- src/minecraft/cammygames/modjam/items/WDFlameThrower.java
+++ src/minecraft/cammygames/modjam/items/WDFlameThrower.java
@@ -1,13 +1,11 @@
 package cammygames.modjam.items;
 
-import net.minecraft.enchantment.Enchantment;
-import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntitySmallFireball;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.player.ArrowLooseEvent;
 import net.minecraftforge.event.entity.player.ArrowNockEvent;
 import cammygames.modjam.WarDefence;
 import cammygames.modjam.entity.FireBall;
@@ -20,57 +18,60 @@ public class WDFlameThrower  extends Item
 		this.setCreativeTab(WarDefence.WarDefenceItems);
 	}
 
-	public boolean lavaBucket = false;
-	private int countparticles = 0;
-
-	 @Override
-    public void onUsingItemTick(ItemStack itemStack, EntityPlayer player, int count) 
+	/**
+	* Called whenever this item is equipped and the right mouse button is pressed. Args: itemStack, world, entityPlayer
+	*/
+	@Override
+	public ItemStack onItemRightClick(ItemStack itemStack, World world, EntityPlayer player) 
 	{
-		World world = player.worldObj;
-	    boolean flag = player.capabilities.isCreativeMode || EnchantmentHelper.getEnchantmentLevel(Enchantment.infinity.effectId, itemStack) > 0;
+		ArrowNockEvent event = new ArrowNockEvent(player, itemStack);
+		MinecraftForge.EVENT_BUS.post(event);
+		if (event.isCanceled()) 
+		{
+			return event.result;
+		}
 	
-	    if (flag || player.inventory.hasItem(Item.fireballCharge.itemID)) {
-	        double yaw = Math.toRadians(player.rotationYaw);
-	        double pitch = Math.toRadians(player.rotationPitch);
+		if (player.capabilities.isCreativeMode || player.inventory.hasItem(Item.fireballCharge.itemID)) 
+		{
+			player.setItemInUse(itemStack, this.getMaxItemUseDuration(itemStack));
+		}
 	
-	        double xDirection = -Math.sin(yaw) * Math.cos(pitch);
-	        double yDirection = -Math.sin(pitch);
-	        double zDirection = Math.cos(yaw) * Math.cos(pitch);
-	
-	        double x = player.posX + xDirection * 2;
-	        double y = player.posY + 1.5 + yDirection;
-	        double z = player.posZ + zDirection * 2;
-	
-	        FireBall fireball = new FireBall(world, x, y, z, xDirection, yDirection, zDirection);
-	
-	        int power = EnchantmentHelper.getEnchantmentLevel(Enchantment.power.effectId, itemStack);
-	        if (power > 0) {
-	            fireball.setDamage(fireball.getDamage() + (double) power * 0.5D + 0.5D);
-	            if (power == 5) {
-	                fireball.setExplosive(true);
-	            }
-	        }
-	
-	        int knockback = EnchantmentHelper.getEnchantmentLevel(Enchantment.punch.effectId, itemStack);
-	        if (knockback > 0) {
-	            fireball.setKnockbackStrength(knockback);
-	        }
-	
-	        if (EnchantmentHelper.getEnchantmentLevel(Enchantment.flame.effectId, itemStack) > 0) {
-	            fireball.setFire(100);
-	        }
-	
-	        itemStack.damageItem(1, player);
-	        world.playAuxSFX(1009, (int) x, (int) y, (int) z, 0);
-	
-	        if (!flag) {
-	            player.inventory.consumeInventoryItem(Item.fireballCharge.itemID);
-	        }
-	
-	        if (!world.isRemote) {
-	            world.spawnEntityInWorld(fireball);
-	        }
-	    }
+		return itemStack;
 	}
 	
+	/**
+	* Called each tick while using an item.
+	*
+	* @param stack The Item being used
+	* @param player The Player using the item
+	* @param count The amount of time in tick the item has been used for
+	* continuously
+	*/
+	@Override
+	public void onUsingItemTick(ItemStack itemStack, EntityPlayer player, int count) 
+	{
+		
+		if (player.inventory.hasItem(Item.fireballCharge.itemID)) 
+    	{
+			World world = player.worldObj;
+			
+			double yaw = Math.toRadians(player.rotationYaw);
+			double pitch = Math.toRadians(player.rotationPitch);
+	
+			double xDirection = -Math.sin(yaw) * Math.cos(pitch);
+			double yDirection = -Math.sin(pitch);
+			double zDirection = Math.cos(yaw) * Math.cos(pitch);
+	
+			double x = player.posX;
+			double y = player.posY;
+			double z = player.posZ;
+	
+			FireBall fireball = new FireBall(world, x, y, z, xDirection, yDirection, zDirection);		
+
+			player.inventory.consumeInventoryItem(Item.fireballCharge.itemID);
+	    	world.spawnParticle("smoke", player.posX, player.posY, player.posZ, 0.0D, 0.0D, 0.0D);
+			world.spawnEntityInWorld(fireball);			
+    	}
+    		 
+	}	
 }
