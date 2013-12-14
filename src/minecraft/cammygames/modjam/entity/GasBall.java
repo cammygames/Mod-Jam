6dcb584555f1cce1124c66f27f44414c09ecff13
cammygames <cammygames@icloud.com>
2013-12-14 12:23:37 +0000

    Other

--- src/minecraft/cammygames/modjam/entity/GasBall.java
+++ src/minecraft/cammygames/modjam/entity/GasBall.java
@@ -97,7 +97,7 @@ public class GasBall extends EntitySmallFireball
                  {	
                 	 for(int g = 0; g < 20; g++) 
                 	 	for(int i = 0; i < 10; i++)
-                                 worldObj.spawnParticle("largesmoke", posX + rand.nextGaussian(), posY + rand.nextGaussian(), posZ + rand.nextGaussian(), 0F, 0F, 0F);
+                                 worldObj.spawnParticle("largesmoke", posX + rand.nextGaussian(), posY + 1, posZ + rand.nextGaussian(), 0F, 0F, 0F);
                 }
          }
          
